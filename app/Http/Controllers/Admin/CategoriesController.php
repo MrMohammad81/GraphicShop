5baeaa57847c4de9dc2a4c7ab0a6328ec13aa0a0
Mr-Mohammad <mohammadhoseinmohit78@gmail.com>
2022-11-27 17:27:11 +0330

    categories feature finished

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Categories\StoreRequests;
+use App\Http\Requests\Admin\Categories\UpdateRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -34,4 +35,34 @@ class CategoriesController extends Controller
         return view('admin.categories.all',compact('categories'));
     }
 
+    public function delete($category_id)
+    {
+        $category = Category::find($category_id);
+
+        $category->delete();
+
+        return back()->with('success' , 'دسته بندی با موفقیت حذف شد');
+    }
+
+    public function edit($category_id)
+    {
+        $categories = Category::find($category_id);
+
+        return view('admin.categories.edit' , compact('categories'));
+    }
+
+    public function update(UpdateRequest $request , $category_id)
+    {
+        $validatedData = $request->validated();
+        $category = Category::find($category_id);
+
+       $updatedCategory =  $category->update([
+            'title' => $validatedData['title'],
+            'slug' => $validatedData['slug']
+        ]);
+
+        if (!$updatedCategory)
+            return back()->with('filed' , 'دسته بندی بروزرسانی نشد');
+        return back()->with('success' , 'دسته بندی با موفقیت بروزرسانی شد');
+    }
 }
